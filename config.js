87f75597586961b997490585fb401aa794b4997c
sadiyasaiya36-netizen <sadiyasaiya36@gmail.com>
2026-06-14 14:10:53 +0530

    Update SESSION_ID default value in config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
+  SESSION_ID: process.env.SESSION_ID || "https://github.com/sadiyasaiya36-netizen/SadiyaBOT.git",
   MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
   OWNER_NUM: process.env.OWNER_NUM || "94729581028",
 };
