9e656fd60944db46440b12e12eb280409ef81e72
sadiyasaiya36-netizen <sadiyasaiya36@gmail.com>
2026-06-14 15:16:25 +0530

    Update OWNER_NUM default value in config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = "true") {
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "sRtHSarK#uCDLUUMBLS0omJbZNhvYBNFMwGotMy1wgW50GXifVCk",
   MONGODB: process.env.MONGODB || "mongodb://mongo:vZVLTItrftcStrwqNgeozAWFituNnycx@mongodb.railway.internal:27017",
-  OWNER_NUM: process.env.OWNER_NUM || "94729581028",
+  OWNER_NUM: process.env.OWNER_NUM || "9472958102",
 };
