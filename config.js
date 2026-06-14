fcd7331ce4359ed09608ec04d8a2f9db771d755c
sadiyasaiya36-netizen <sadiyasaiya36@gmail.com>
2026-06-14 15:15:24 +0530

    0

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "https://github.com/sadiyasaiya36-netizen/SadiyaBOT.git",
+  SESSION_ID: process.env.SESSION_ID || "sRtHSarK#uCDLUUMBLS0omJbZNhvYBNFMwGotMy1wgW50GXifVCk",
   MONGODB: process.env.MONGODB || "mongodb://mongo:vZVLTItrftcStrwqNgeozAWFituNnycx@mongodb.railway.internal:27017",
   OWNER_NUM: process.env.OWNER_NUM || "94729581028",
 };
